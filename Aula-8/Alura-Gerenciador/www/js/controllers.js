8950e37694b621b2f0939ae6f00da27ca638cc88
Lazaro Jr <linkout.lazaro@gmail.com>
2016-10-19 13:41:28 -0200

    Aula 8

---
+++ Aula-8/Alura-Gerenciador/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('AgendamentoController', function($scope, GerenciadorService){
+
+	$scope.agendamentos = [];
+
+	GerenciadorService.obterAgendamentos().then(function(dados){
+		$scope.agendamentos = dados;
+	})
+
+})
+   
+.controller('FornecedorController', function($scope, GerenciadorService){
+
+	$scope.fornecedores = [];
+
+	GerenciadorService.obterFornecedores().then(function(dados){
+		$scope.fornecedores = dados;
+	})
+
+})
+    
