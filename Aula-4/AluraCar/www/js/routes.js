db6ab0644791f797a7a521c786eed440c93203be
Lazaro Jr <linkout.lazaro@gmail.com>
2016-10-19 12:05:29 -0200

    Aula 4

---
+++ Aula-4/AluraCar/www/js/routes.js
@@ -0,0 +1,53 @@
+angular.module('starter')
+.config(function($stateProvider, $urlRouterProvider){
+
+$urlRouterProvider.otherwise('login');
+
+$stateProvider
+
+.state('app', {
+	url : '/app',
+	templateUrl : 'templates/menu.html',
+	abstract: true,
+	controller: 'MenuController'
+})
+
+.state('app.perfil', {
+	url : '/perfil',
+	views : {
+		'menuContent' : {
+			templateUrl : 'templates/perfil.html',
+			controller : 'PerfilController'
+		}
+	}
+})
+
+.state('app.listagem',{
+	url : '/listagem',
+	views : {
+		'menuContent' : {
+			templateUrl : 'templates/listagem.html',
+			controller: 'ListagemController'
+		}
+	}
+})
+
+.state('carroescolhido',{
+	url : '/carroescolhido/:carro',
+	templateUrl: 'templates/carroescolhido.html',
+	controller: 'CarroEscolhidoController'
+})
+
+.state('finalizarpedido',{
+	url : '/finalizarpedido/:carro',
+	templateUrl : 'templates/finalizarpedido.html',
+	controller : 'FinalizarPedidoController'
+})
+
+.state('login', {
+	url : '/login',
+	templateUrl : 'templates/login.html',
+	controller : 'LoginController'
+})
+
+})
